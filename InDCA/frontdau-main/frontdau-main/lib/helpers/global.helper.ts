68a5627b34c740b7a5b5f313501ffc91c9895070
maksym784 <117281633+maksym784@users.noreply.github.com>
2023-10-02 23:46:09 +0200

    Delete InDCA directory

--- InDCA/frontdau-main/frontdau-main/lib/helpers/global.helper.ts
+++
@@ -1,9 +0,0 @@
-export const copyToClipboard = async (text?: string): Promise<void> => {
-  try {
-    if (text) {
-      return navigator.clipboard.writeText(text);
-    }
-  } catch (error) {
-    throw new Error("Error while copying address");
-  }
-};
